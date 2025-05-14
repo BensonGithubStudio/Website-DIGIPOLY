54ae08a8417b11eaa69e0316f16132f9a12b04d3
Benson12332622 <Benson46459937@gmail.com>
2025-05-14 15:13:00 +0800

    修改背景顏色

--- javascript/effect.js
+++ javascript/effect.js
@@ -1,24 +1,47 @@
-//h1和p的文字效果
+// h1 和 p 的文字效果
 document.addEventListener("DOMContentLoaded", function () {
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      const group = entry.target.parentElement.querySelectorAll('h1, p');
+    const textObserver = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            const group = entry.target.parentElement.querySelectorAll('h1, p');
 
-      if (entry.isIntersecting) {
-        group.forEach((el, index) => {
-          el.style.transitionDelay = `${index * 0.2}s`;
-          el.classList.add('visible');
+            if (entry.isIntersecting) {
+                group.forEach((el, index) => {
+                    el.style.transitionDelay = `${index * 0.2}s`;
+                    el.classList.add('visible');
+                });
+            } else {
+                group.forEach(el => {
+                    el.style.transitionDelay = '0s';
+                    el.classList.remove('visible');
+                });
+            }
         });
-      } else {
-        group.forEach(el => {
-          el.style.transitionDelay = '0s';
-          el.classList.remove('visible');
-        });
-      }
+    }, {
+      threshold: 0.2
     });
-  }, {
-    threshold: 0.2 // 控制幾成進入畫面時觸發
-  });
 
-  document.querySelectorAll("h1").forEach(el => observer.observe(el));
+    document.querySelectorAll("h1").forEach(el => textObserver.observe(el));
+});
+
+// 橫幅消失時，變深色模式
+document.addEventListener("DOMContentLoaded", () => {
+    const banner = document.querySelector("#banner");
+    const body = document.body;
+    const header = document.querySelector("header");
+
+    const bannerObserver = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                body.classList.add("change-mode");
+                header.classList.add("change-mode");
+            } else {
+                body.classList.remove("change-mode");
+                header.classList.remove("change-mode");
+            }
+        });
+    }, {
+        threshold: 0
+    });
+
+    bannerObserver.observe(banner);
 });
